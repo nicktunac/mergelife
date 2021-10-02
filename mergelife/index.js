94cbd3b093509c73bfc3e44800d8a7f55714b6ce
Dominick Tunac <dominick.tunac@gmail.com>
2021-10-02 21:42:01 +0200

    initial commit:

---
+++ mergelife/index.js
@@ -0,0 +1,44 @@
+const canvas1 = document.getElementById('canvas-1')
+window.ml1 = new MergeLifeRender()
+window.ml1.init({rule: '6Eb6-ba3d-70b4-ac6f-baae-2604-8529-8998', canvas: canvas1, cellSize: 2, controls: false, resetCount: 1000})
+window.ml1.startAnimation()
+
+const canvas2 = document.getElementById('canvas-2')
+window.ml2 = new MergeLifeRender()
+window.ml2.init({rule: 'ea44-55dF-9025-bEad-5f6e-45ca-6168-275a', canvas: canvas2, cellSize: 2, controls: false, resetCount: 1000})
+window.ml2.startAnimation()
+
+const canvas3 = document.getElementById('canvas-3')
+window.ml3 = new MergeLifeRender()
+window.ml3.init({rule: '1c48-9004-8831-41be-2804-8f50-9901-db18', canvas: canvas3, cellSize: 2, controls: false, resetCount: 1000})
+window.ml3.startAnimation()
+
+const canvas4 = document.getElementById('canvas-4')
+window.ml3 = new MergeLifeRender()
+window.ml3.init({rule: 'c168-7b61-b5cc-4e64-8f7a-df90-5362-8750', canvas: canvas4, cellSize: 2, controls: false, resetCount: 1000})
+window.ml3.startAnimation()
+
+const canvas5 = document.getElementById('canvas-5')
+window.ml3 = new MergeLifeRender()
+window.ml3.init({rule: 'd106-f969-cda8-ceb6-9964-977c-cc43-62b1', canvas: canvas5, cellSize: 2, controls: false, resetCount: 1000})
+window.ml3.startAnimation()
+
+const canvas6 = document.getElementById('canvas-6')
+window.ml3 = new MergeLifeRender()
+window.ml3.init({rule: '2152-9b71-abb7-162a-45ff-dd03-fe15-957e', canvas: canvas6, cellSize: 2, controls: false, resetCount: 1000})
+window.ml3.startAnimation()
+
+const canvas7 = document.getElementById('canvas-7')
+window.ml3 = new MergeLifeRender()
+window.ml3.init({rule: '6007-7d42-05e5-1b9b-2899-e043-1cd4-2f7b', canvas: canvas7, cellSize: 2, controls: false, resetCount: 1000})
+window.ml3.startAnimation()
+
+const canvas8 = document.getElementById('canvas-8')
+window.ml3 = new MergeLifeRender()
+window.ml3.init({rule: '6769-5dd6-7d03-564e-a5ec-cae2-54c4-810c', canvas: canvas8, cellSize: 2, controls: false, resetCount: 1000})
+window.ml3.startAnimation()
+
+const canvas9 = document.getElementById('canvas-9')
+window.ml3 = new MergeLifeRender()
+window.ml3.init({rule: 'cb97-6a74-88c0-28aa-1b6a-834b-4fe8-60ac', canvas: canvas9, cellSize: 2, controls: false, resetCount: 1000})
+window.ml3.startAnimation()
